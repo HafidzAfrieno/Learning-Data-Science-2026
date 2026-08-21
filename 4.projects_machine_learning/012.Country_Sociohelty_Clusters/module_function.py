7113fe0e6e13a35f34abd816c2b3c620ef9e4c5d
HafidzAfreieno <hafidzprogramer@gmail.com>
2026-08-21 08:57:56 +0700

    update code

--- 4.projects_machine_learning/012.Country_Sociohelty_Clusters/module_function.py
+++ 4.projects_machine_learning/012.Country_Sociohelty_Clusters/module_function.py
@@ -7,3 +7,12 @@ from sklearn.preprocessing import MinMaxScaler
 from sklearn.decomposition import PCA
 from sklearn.cluster import DBSCAN
 from sklearn.metrics import silhouette_score, calinski_harabasz_score, davies_bouldin_score
+
+class DbscanClustering:
+    """
+    Kelas inti untuk menangani logika pemodelan, training, perhitungan metrik evaluasi,
+    serta transformasi data untuk segmentasi menggunakan DBSCAN.
+    """
+    def __init__(self):
+        pass
+    
